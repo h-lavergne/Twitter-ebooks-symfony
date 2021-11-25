052c8f359f04a7b69848a812b17b4c8f28a22f0f
hugo <hugo.lavergne@edu.devinci.fr>
2021-11-25 12:37:51 +0100

    add process pacjage + try starting node script

--- src/Controller/TwitterController.php
+++ src/Controller/TwitterController.php
@@ -4,18 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Process\Process;
+
 
 class TwitterController extends AbstractController
 {
     /**
      * @Route("/start-ebooks", name="twitter")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TwitterController.php',
-        ]);
+
+        $pathToScript = "/var/www/twitter_ebooks";
+        $process = new Process(['node -r esm test.js', $pathToScript]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
     }
 }
